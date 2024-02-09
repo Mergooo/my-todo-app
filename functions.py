9d72fe4c137e14597844a085aa29892f06c1a9fd
Mergooo <Mergoo@protonmail.com>
2024-02-09 16:47:24 +0100

    test3

--- functions.py
+++ functions.py
@@ -8,7 +8,6 @@ def get_todos(filepath=FILEPATH):
         todos_local = file_local.readlines()
     return todos_local
 
-
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, "w") as file:
